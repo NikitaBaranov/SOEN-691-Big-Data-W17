4a237bf09b1e95d14f8a9e582353dc2dbd12abd1
Nikita Baranov <NikitaBaranov@gmail.com>
2017-04-15 04:46:51 +0500

    Tiding up

--- Lab1/trynewskip.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-hdfs dfs -rm /user/Nikita/skip.txt
-hdfs dfs -put skip.txt /user/Nikita/skip.txt
-hdfs dfs -rm -r /user/Nikita/output-alice-skip
-hadoop jar wordcount2.jar WordCount2 alice.txt output-alice-skip -skip skip.txt -D wordcount.case.sensitive=false
--- src/main/java/Kmeans.java
+++
@@ -1,7 +0,0 @@
-package PACKAGE_NAME;
-
-/**
- * Created by Nikita on 16.02.17.
- */
-public class Kmeans {
-}
